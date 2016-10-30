3fc496cd57ee2d5a0ebb1b34244742f1dd441cc0
Christian Kienle <me@christian-kienle.de>
2016-10-30 17:54:13 +0100

    iOS target

---
+++ Bold-iOS/Bold-iOS-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Bold-iOS-Bridging-Header.h
+//  Bold
+//
+//  Created by cmk on 30/10/2016.
+//  Copyright © 2016 Christian Kienle. All rights reserved.
+//
+
+#ifndef Bold_iOS_Bridging_Header_h
+#define Bold_iOS_Bridging_Header_h
+#include "Bold-iOS.h"
+#endif /* Bold_iOS_Bridging_Header_h */
---
+++ Bold-iOS/Bold-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Bold-iOS.h
+//  Bold-iOS
+//
+//  Created by cmk on 30/10/2016.
+//  Copyright © 2016 Christian Kienle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bold-iOS.
+FOUNDATION_EXPORT double Bold_iOSVersionNumber;
+
+//! Project version string for Bold-iOS.
+FOUNDATION_EXPORT const unsigned char Bold_iOSVersionString[];
+
+#include <sqlite3.h>
+
+
